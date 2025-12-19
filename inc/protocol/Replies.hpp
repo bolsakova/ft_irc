59fb671fa4172bc9e4e81694efc23905a4d634b5
bolsakova <bolsakova@github.com>
2025-12-19 18:31:27 +0100

    created header with all replies for IRC

---
+++ inc/protocol/Replies.hpp
@@ -0,0 +1,25 @@
+#ifndef REPLIES_HPP
+#define REPLIES_HPP
+
+// Successful replies (001-099)
+#define RPL_WELCOME         001
+#define RPL_YOURHOST        002
+#define RPL_CREATED         003
+#define RPL_MYINFO          004
+
+// Error replies (400-599)
+#define ERR_NOSUCHNICK      401
+#define ERR_NOSUCHCHANNEL   403
+#define ERR_CANNOTSENDTOCHAN 404
+#define ERR_NORECIPIENT     411
+#define ERR_NOTEXTTOSEND    412
+#define ERR_UNKNOWNCOMMAND  421
+#define ERR_NONICKNAMEGIVEN 431
+#define ERR_ERRONEUSNICKNAME 432
+#define ERR_NICKNAMEINUSE   433
+#define ERR_NOTREGISTERED   451
+#define ERR_NEEDMOREPARAMS  461
+#define ERR_ALREADYREGISTRED 462
+#define ERR_PASSWDMISMATCH  464
+
+#endif
